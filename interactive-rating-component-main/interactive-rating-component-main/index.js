00d23f2b12d16746546a76b3be68e625c4133389
bigNath500 <nathanjoe.git@gmail.com>
2022-08-29 22:45:42 +0100

    project completed

--- interactive-rating-component-main/interactive-rating-component-main/index.js
+++ interactive-rating-component-main/interactive-rating-component-main/index.js
@@ -14,6 +14,7 @@
         let appricText = document.getElementById("appricText");
         let mainText = document.getElementById("mainText");
         let subText = document.getElementById("subText");
+        let rating_circles = document.querySelector(".rating_circles");
 
         starBox.classList.add("d-none")
         mainText.classList.add("d-none")
@@ -23,4 +24,89 @@
         appricText.classList.remove("d-none");
         valueSubmitter.classList.remove("d-none")
         valueSubmitter.innerText= "You selected 1 out of 5"
+        rating_circles.classList.add("d-none")
+    });
+
+    ratingTwoBtn.addEventListener('click', () => {
+        let starBox = document.getElementById("starBox");
+        let mobileIllustrator = document.getElementById("mobileIllustrator");
+        let valueSubmitter = document.getElementById("valueSubmitter");
+        let questText = document.getElementById("questText");
+        let appricText = document.getElementById("appricText");
+        let mainText = document.getElementById("mainText");
+        let subText = document.getElementById("subText");
+        let rating_circles = document.querySelector(".rating_circles");
+
+        starBox.classList.add("d-none")
+        mainText.classList.add("d-none")
+        questText.classList.add("d-none")
+        mobileIllustrator.classList.remove("d-none");
+        subText.classList.remove("d-none");
+        appricText.classList.remove("d-none");
+        valueSubmitter.classList.remove("d-none")
+        valueSubmitter.innerText= "You selected 2 out of 5"
+        rating_circles.classList.add("d-none")
+    });
+
+    ratingThreeBtn.addEventListener('click', () => {
+        let starBox = document.getElementById("starBox");
+        let mobileIllustrator = document.getElementById("mobileIllustrator");
+        let valueSubmitter = document.getElementById("valueSubmitter");
+        let questText = document.getElementById("questText");
+        let appricText = document.getElementById("appricText");
+        let mainText = document.getElementById("mainText");
+        let subText = document.getElementById("subText");
+        let rating_circles = document.querySelector(".rating_circles");
+
+        starBox.classList.add("d-none")
+        mainText.classList.add("d-none")
+        questText.classList.add("d-none")
+        mobileIllustrator.classList.remove("d-none");
+        subText.classList.remove("d-none");
+        appricText.classList.remove("d-none");
+        valueSubmitter.classList.remove("d-none")
+        valueSubmitter.innerText= "You selected 3 out of 5"
+        rating_circles.classList.add("d-none")
+    });
+
+    ratingFourBtn.addEventListener('click', () => {
+        let starBox = document.getElementById("starBox");
+        let mobileIllustrator = document.getElementById("mobileIllustrator");
+        let valueSubmitter = document.getElementById("valueSubmitter");
+        let questText = document.getElementById("questText");
+        let appricText = document.getElementById("appricText");
+        let mainText = document.getElementById("mainText");
+        let subText = document.getElementById("subText");
+        let rating_circles = document.querySelector(".rating_circles");
+
+        starBox.classList.add("d-none")
+        mainText.classList.add("d-none")
+        questText.classList.add("d-none")
+        mobileIllustrator.classList.remove("d-none");
+        subText.classList.remove("d-none");
+        appricText.classList.remove("d-none");
+        valueSubmitter.classList.remove("d-none")
+        valueSubmitter.innerText= "You selected 4 out of 5"
+        rating_circles.classList.add("d-none")
+    });
+
+    ratingFiveBtn.addEventListener('click', () => {
+        let starBox = document.getElementById("starBox");
+        let mobileIllustrator = document.getElementById("mobileIllustrator");
+        let valueSubmitter = document.getElementById("valueSubmitter");
+        let questText = document.getElementById("questText");
+        let appricText = document.getElementById("appricText");
+        let mainText = document.getElementById("mainText");
+        let subText = document.getElementById("subText");
+        let rating_circles = document.querySelector(".rating_circles");
+
+        starBox.classList.add("d-none")
+        mainText.classList.add("d-none")
+        questText.classList.add("d-none")
+        mobileIllustrator.classList.remove("d-none");
+        subText.classList.remove("d-none");
+        appricText.classList.remove("d-none");
+        valueSubmitter.classList.remove("d-none")
+        valueSubmitter.innerText= "You selected 5 out of 5"
+        rating_circles.classList.add("d-none")
     });
